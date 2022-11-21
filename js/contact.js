b8f708734584a8e9c8f4f4e905adbbe41eabb289
Hayden Burnell <71723408+hburne11@users.noreply.github.com>
2022-11-20 19:32:48 -0500

    ~

---
+++ js/contact.js
@@ -0,0 +1,42 @@
+import { SendMail } from "./components/mailer.js";
+
+(() => {
+    const { createApp } = Vue
+
+    createApp({
+        data() {
+            return {
+                message: 'Hello Vue!'
+            }
+        },
+
+        methods: {
+            processMailFailure(result) {
+                // shows a failure message in the UI
+                // use this.$refs to connect to the elements on the page and mark any empty fields/inputs with an error class
+                this.$refs.status.style.display = "block";
+                this.$refs.status.innerHTML = "";
+                this.$refs.status.innerHTML = "Error! Please check the form and try again.";
+                message.forEach(field => this.$refs[field].classList.add('error'));
+                console.log(result)
+                // show some errors in the UI here to let the user know the mail attempt was successful
+            },
+
+            processMailSuccess(result) {
+                // show a success message in the UI
+                this.$refs.status.style.display = "block";
+                this.$refs.status.innerHTML = "";
+                this.$refs.status.innerHTML = "Thank you, I will be in contact shortly.";
+                console.log(result)
+                // show some UI here to let the user know the mail attempt was successful
+            },
+
+            processMail(event) {        
+                // use the SendMail component to process mail
+                SendMail(this.$el.parentNode)
+                    .then(data => this.processMailSuccess(data))
+                    .catch(err => this.processMailFailure(err));
+            }
+        }
+    }).mount('#mail-form')
+})();
